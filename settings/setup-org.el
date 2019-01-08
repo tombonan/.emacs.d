143d7c567f030c5afe880e2b16e504ae7ee3159a
tombonan <tombonan018@gmail.com>
2019-01-07 23:56:44 -0700

    Set default notes file for org mode

--- settings/setup-org.el
+++ settings/setup-org.el
@@ -4,5 +4,6 @@
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (setq org-directory "~/Dropbox/org")
+(setq org-default-notes-file (concat org-directory "/notes.org"))
 
 (provide 'setup-org)
