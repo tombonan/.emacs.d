0a55d04a8408fe4576949eb8c14d71465489771b
tombonan <tombonan018@gmail.com>
2019-06-17 15:43:35 -0600

    Return projectile results by recetly opened files

--- settings/setup-projectile.el
+++ settings/setup-projectile.el
@@ -3,5 +3,8 @@
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
+;; Sort files by recently acitve buffers, then recently opened files
+(setq projectile-sort-order 'recently-active)
+
 
 (provide 'setup-projectile)
