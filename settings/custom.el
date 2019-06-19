fd803c1154613b294194bd500ee6806216173101
tombonan <tombonan018@gmail.com>
2019-06-19 11:08:02 -0600

    Clean up custom-set-variables

---
+++ settings/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "944e706816bc594367bc16a745b746a38aecc55b9ed5b5209e05289df22a5c52" default)))
+ '(magit-dispatch-arguments nil)
+ '(package-selected-packages
+   (quote
+    (robe helm-swoop markdown-mode org-bullets helm-flyspell fireplace smartparens rjsx-mode undo-tree multiple-cursors magit restclient neotree direx emmet-mode helm-ag helm-projectile avy web-mode projectile-rails solarized-theme god-mode helm helm-ebdb))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line-buffer-id ((t (:foreground "cornsilk" :weight bold))))
+ '(variable-pitch ((t (:family "Source Sans Pro" :weight regular)))))
