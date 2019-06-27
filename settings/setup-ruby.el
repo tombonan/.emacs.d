d962d4da466e4dc952c6f3aaacbdf02d30eef8e8
tombonan <tombonan018@gmail.com>
2019-06-27 15:45:32 -0600

    Add ruby block and update ruby hooks

---
+++ settings/setup-ruby.el
@@ -0,0 +1,12 @@
+;; Ruby Block Highlighting
+(require 'ruby-block)
+(ruby-block-mode t)
+
+;; do overlay
+(setq ruby-block-highlight-toggle 'overlay)
+;; display to minibuffer
+(setq ruby-block-highlight-toggle 'minibuffer)
+;; display to minibuffer and do overlay
+(setq ruby-block-highlight-toggle t)
+
+(provide 'setup-ruby)
