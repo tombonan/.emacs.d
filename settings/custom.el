9e2b0873bf435ac93fdc398ec4ec461d19462885
Tom Bonan <tombonan018@gmail.com>
2021-07-21 16:42:34 -0600

    Bump restclient mode version

--- settings/custom.el
+++ settings/custom.el
@@ -5,12 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
  '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "944e706816bc594367bc16a745b746a38aecc55b9ed5b5209e05289df22a5c52" default)))
+   '("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "944e706816bc594367bc16a745b746a38aecc55b9ed5b5209e05289df22a5c52" default))
  '(magit-dispatch-arguments nil)
  '(package-selected-packages
-   (quote
-    (magit json-mode flycheck arduino-mode expand-region typescript-mode helm-tramp clj-refactor clojure-mode-extra-font-locking cider go-mode groovy-mode yaml-mode docker-tramp dockerfile-mode enh-ruby-mode coffee-mode helm-swoop markdown-mode org-bullets helm-flyspell fireplace smartparens rjsx-mode undo-tree multiple-cursors restclient neotree direx emmet-mode helm-ag helm-projectile avy web-mode projectile-rails solarized-theme god-mode helm helm-ebdb))))
+   '(restclient magit json-mode flycheck arduino-mode expand-region typescript-mode helm-tramp clj-refactor clojure-mode-extra-font-locking cider go-mode groovy-mode yaml-mode docker-tramp dockerfile-mode enh-ruby-mode coffee-mode helm-swoop markdown-mode org-bullets helm-flyspell fireplace smartparens rjsx-mode undo-tree multiple-cursors neotree direx emmet-mode helm-ag helm-projectile avy web-mode projectile-rails solarized-theme god-mode helm helm-ebdb)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
