f890df6c0c31382e67e4862085ef9fc25cff742c
tombonan <tombonan018@gmail.com>
2023-03-01 10:21:04 -0700

    Update all packages

---
+++ elpa/fireplace-20200402.2206/fireplace-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from fireplace.el  -*- no-byte-compile: t -*-
+(define-package "fireplace" "20200402.2206" "A cozy fireplace for emacs" 'nil :commit "f6c23e259349922aae25cf2898ba815a7d8f2527" :authors '(("Johan Sivertsen" . "johanvts@gmail.com")) :maintainer '("Johan Sivertsen" . "johanvts@gmail.com") :keywords '("games") :url "https://github.com/johanvts/emacs-fireplace")
