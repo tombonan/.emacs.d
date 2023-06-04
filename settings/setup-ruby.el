157513855dc3f117021742e750c94d2db21847f5
tombonan <tombonan018@gmail.com>
2023-06-03 22:07:52 -0600

    Add function to open an spec file from current buffer

--- settings/setup-ruby.el
+++ settings/setup-ruby.el
@@ -21,4 +21,36 @@
 (setq ruby-insert-encoding-magic-comment nil)
 ;; (setq enh-ruby-add-encoding-comment-on-save nil)
 
+;; Custom Ruby navigation helpers
+(defvar clean-relative-path-mappings
+  '(("^app/" . "")
+    ("controllers/" . "requests/"))
+  "Mappings used for cleaning relative paths in `clean-relative-path`.")
+
+(defun clean-relative-path (relative-path)
+  "Remap the relative path of a file in app/ to a corresponding relative filepath for an
+   RSpec test file."
+  (let ((cleaned-path relative-path))
+    (dolist (mapping clean-relative-path-mappings)
+      (setq cleaned-path (replace-regexp-in-string (car mapping) (cdr mapping) cleaned-path)))
+    cleaned-path))
+
+(defun tom/open-rspec-file ()
+  "Opens a corresponding RSpec file in the buffer.
+
+   Assumes:
+     - Project uses proectile
+     - Tests are placed in the /spec directory of the project root
+     - Controller specs are namespaced in a 'requests' directory
+  "
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (if (string-match "\\(.*\\)\\.rb$" file-name)
+        (let* ((base-name (match-string 1 file-name))
+               (project-root (projectile-project-root))
+               (relative-path (file-relative-name base-name project-root))
+               (test-path (concat "spec/" (clean-relative-path relative-path) "_spec.rb")))
+          (find-file (expand-file-name test-path project-root)))
+      (message "Not a Ruby file."))))
+
 (provide 'setup-ruby)
