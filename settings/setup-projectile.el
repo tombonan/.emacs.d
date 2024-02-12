1c0ad582e81748d38dfe1f1e1c9f43a96d39613a
tombonan <tombonan018@gmail.com>
2024-02-12 10:58:52 -0700

    Fix bug where projectile ignore directories are incorrectly indexing

--- settings/setup-projectile.el
+++ settings/setup-projectile.el
@@ -11,6 +11,8 @@
 (setq projectile-ignored-project-function #'tom/projectile-ignore-tmp-projects)
 (setq projectile-require-project-root nil)
 (setq projectile-enable-caching t)
+(setq projectile-indexing-method 'native)
+
 (setq projectile-globally-ignored-directories
       (append '(
         ".git"
