88178578d145a491d99347eebc889a2fbb0a6225
RC-tombonan <tom.bonan@redcanary.com>
2026-06-17 15:47:31 -0600

    Get rid of cron stuff now that I can use crontab

--- settings/cron.el
+++ settings/cron.el
@@ -1,19 +1,11 @@
 ;; Custom cron-like actions
 
-;; Screenshot cleanup
-(when (file-executable-p "/Users/tombonan/github/dotfiles/bin/screenshot-cleaner")
-  (defun tom/screenshot-clean ()
-    (shell-command "/Users/tombonan/github/dotfiles/bin/screenshot-cleaner"))
+;; Screenshot cleaner. Only run if crontab is not accessible
+;; (when (file-executable-p "/Users/tombonan/github/dotfiles/bin/screenshot-cleaner")
+;;   (defun tom/screenshot-clean ()
+;;     (shell-command "/Users/tombonan/github/dotfiles/bin/screenshot-cleaner"))
 
-  ;; Run every 15 min
-  (run-at-time "0 min" (* 15 60) 'tom/screenshot-clean))
-
-;; Claude code backups
-(when (file-executable-p "/Users/tombonan/github/dotfiles/bin/backup-claude-configs")
-  (defun tom/claude-backup ()
-    (shell-command "/Users/tombonan/github/dotfiles/bin/backup-claude-configs"))
-
-  ;; Run every 15 min
-  (run-at-time "0 min" (* 3 60 60) 'tom/claude-backup))
+;;   ;; Run every 15 min
+;;   (run-at-time "0 min" (* 15 60) 'tom/screenshot-clean))
 
 (provide 'cron)
